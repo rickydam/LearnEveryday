65cad2d602b1d8b1cba3a3d52952986c2e5e2d09
Ricky Dam <ricky.dam10@hotmail.com>
2017-05-21 22:55:51 -0400

    Added a check to prevent blank inputs

--- controllers/insertController.js
+++ controllers/insertController.js
@@ -2,20 +2,32 @@ var app = angular.module('myApp', []);
 
 app.controller('insertController', function($scope, $http, resultsService) {
   $scope.insertdata = function() {
-    $http({
-      method: 'POST',
-      url: 'http://localhost/storestuff/insert.php',
-      data: {'date':$scope.date, 'content':$scope.content, 'in':'json-format'},
-      headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-    })
-    .then(function(res) {
-      console.log("Successful response", res);
-      resultsService.addItem(res.data, $scope.date, $scope.content);
-      $scope.date = "";
-      $scope.content = "";
-    })
-    .catch(function(err) {
-      console.error("Error with POST", err);
-    });
+    var dateInput = document.getElementById("dateInput");
+    var contentInput = document.getElementById("contentInput");
+    if(dateInput.value == "") {
+      alert("Date cannot be blank!");
+      dateInput.focus();
+    }
+    else if(contentInput.value == "") {
+      alert("Content cannot be blank!");
+      contentInput.focus();
+    }
+    else {
+      $http({
+        method: 'POST',
+        url: 'http://localhost/storestuff/insert.php',
+        data: {'date':$scope.date, 'content':$scope.content, 'in':'json-format'},
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+      })
+      .then(function(res) {
+        console.log("Successful response", res);
+        resultsService.addItem(res.data, $scope.date, $scope.content);
+        $scope.date = "";
+        $scope.content = "";
+      })
+      .catch(function(err) {
+        console.error("Error with POST", err);
+      });
+    }
   }
 });
